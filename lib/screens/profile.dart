f15394564c8c61a56d04586deef96bffbf171b56
ashahir <ashahir@gmail.com>
2024-09-23 16:25:04 +0800

    profile details

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -11,9 +11,68 @@ class Profile extends StatelessWidget {
           backgroundColor: Theme.of(context).colorScheme.primary,
           foregroundColor: Theme.of(context).colorScheme.onPrimary,
         ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              width: double.infinity,
+              height: 200,
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.primary,
+                borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(50),
+                  bottomRight: Radius.circular(50),
+                )
+              ),
+              child: const Column(
+                children: [
+                  Expanded(
+                    child: CircleAvatar(
+                      minRadius: 20,
+                    ),
+                  ),
+                  Text(
+                    "Online",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                    )
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Text(
+              "Nama: ",
+              style: TextStyle(
+                color: Colors.blue,
+                fontSize: 20,
+              )
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Text(
+              "No. KP: ",
+              style: TextStyle(
+                color: Colors.blue,
+                fontSize: 20,
+              )
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Text(
+              "Alamat: ",
+              style: TextStyle(
+                color: Colors.blue,
+                fontSize: 20,
+              )
+            ),
+          ],
+        )
     );
   }
 }
