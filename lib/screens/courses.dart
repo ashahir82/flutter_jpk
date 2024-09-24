2b470e0c7b36f8e759a1013c8ebe42f32fa2eb4d
ashahir <ashahir@gmail.com>
2024-09-24 10:26:24 +0800

    listview

--- lib/screens/courses.dart
+++ lib/screens/courses.dart
@@ -1,16 +1,38 @@
 import 'package:flutter/material.dart';
 
-class Courses extends StatelessWidget {
+class Courses extends StatefulWidget {
   const Courses({super.key});
 
+  @override
+  State<Courses> createState() => _CoursesState();
+}
+
+class _CoursesState extends State<Courses> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Senarai Kursus'),
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        foregroundColor: Theme.of(context).colorScheme.onPrimary,
       ),
-      body: const Center(
-        child: Text('Hello World'),
+      body: ListView(
+        itemExtent: 50,
+        children: const [
+          ListTile(
+            tileColor: Colors.amber,
+            iconColor: Colors.white,
+            leading: Icon(
+              Icons.book,
+              size: 40,
+            ),
+            trailing: Icon(
+              Icons.arrow_forward_ios,
+              size: 20,
+            ),
+            title: Text('Kursus 1'),
+          ),
+        ],
       ),
     );
   }
